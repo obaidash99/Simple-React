24fa17669e1e2d9801be6aa268446e9d8ce1402e
obaida <obaidashurbaji99@gmail.com>
2022-09-19 23:42:45 +0200

    go to sleep cuz of being tired :)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import './App.css';
+
+// Import components
+import Nav from './components/Nav';
+import Tweets from './components/Tweets';
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Helllo</h1>
+			<h1>Helllo React!</h1>
+			<div className="home">
+				<Nav />
+				<Tweets />
+			</div>
 		</div>
 	);
 }
---
+++ src/components/Tweets.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Tweets = () => (
+	<section>
+		<h1>Tweets</h1>
+	</section>
+);
+
+export default Tweets;
