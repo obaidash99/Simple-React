14703538df01da2dad0a31ca1d405a9375d9a8fa
obaida <obaidashurbaji99@gmail.com>
2022-09-19 23:19:59 +0200

    cleaning up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Helllo</h1>
+		</div>
+	);
 }
 
 export default App;
